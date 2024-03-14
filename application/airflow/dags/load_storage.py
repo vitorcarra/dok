a7b5999e4831ea22bf6c37451cfeff019c45fae0
Vitor Carra <vitor.carra@gmail.com>
2024-03-14 17:13:03 -0300

    fix dag

--- application/airflow/dags/load_storage.py
+++ application/airflow/dags/load_storage.py
@@ -16,18 +16,17 @@ with DAG('download_and_save_to_s3', default_args=default_args, schedule_interval
     download_and_save_legislaturas = HttpToS3Operator(
         task_id='download_and_save_legislaturas',
         http_conn_id='http_conn',
-        path='https://dadosabertos.camara.leg.br/arquivos/legislaturas/csv/legislaturas.csv',
+        endpoint='https://dadosabertos.camara.leg.br/arquivos/legislaturas/csv/legislaturas.csv',
         s3_bucket='your-bucket',
-        s3_key='path/to/save/file/legislaturas.csv',
+        s3_key='s3://landing/legislaturas.csv',
         aws_conn_id='aws_s3_conn'
     )
 
     download_and_save_deputados = HttpToS3Operator(
         task_id='download_and_save_deputados',
         http_conn_id='http_conn',
-        path='https://dadosabertos.camara.leg.br/arquivos/deputados/csv/deputados.csv',
-        s3_bucket='your-bucket',
-        s3_key='path/to/save/file/deputados.csv',
+        endpoint='https://dadosabertos.camara.leg.br/arquivos/deputados/csv/deputados.csv',
+        s3_key='s3://landing/deputados.csv',
         aws_conn_id='aws_s3_conn'
     )
 
