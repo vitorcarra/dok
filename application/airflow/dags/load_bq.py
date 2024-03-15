de9b0bd740d5e3f849e49a258a6a1f7b213e77cb
Vitor Carra <vitor.carra@gmail.com>
2024-03-15 11:59:20 -0300

    load to bq dag

--- application/airflow/dags/load_bq.py
+++ application/airflow/dags/load_bq.py
@@ -1,30 +1,45 @@
 from datetime import datetime
 from airflow import DAG
-from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
-
 from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.providers.google.cloud.hooks.gcs import GCSHook
+from airflow.providers.google.cloud.hooks.bigquery import BigQueryHook
 
 default_args = {
-    'start_date': datetime(2024, 3, 1),
-    'retries': 3,
+    'owner': 'airflow',
+    'start_date': datetime(2024, 3, 12),
 }
 
-with DAG(
-    'load_file_to_bigquery',
-    default_args=default_args,
-    schedule_interval='@daily',
-) as dag:
-    load_to_bigquery = GCSToBigQueryOperator(
-        task_id='load_to_bigquery',
-        bucket='st-landing-bucket',
-        source_objects=['legislatura.csv'],
-        schema_update_options=['ALLOW_FIELD_ADDITION'],
-        destination_project_dataset_table='study-341002.data.legislatura',
-        write_disposition='WRITE_TRUNCATE',
-        autodetect=True,
+def load_gcs_to_bigquery(bucket_name, source_object, dataset_id, table_id):
+    gcs_hook = GCSHook(gcp_conn_id='gcp_conn_id')
+    bigquery_hook = BigQueryHook(
         gcp_conn_id='gcp_conn_id',
+    )
 
+    # Download the file from GCS to a local file
+    with open('/tmp/temp_file.csv', 'wb') as file:
+        file.write(gcs_hook.download(bucket_name, source_object))
 
+    # Load the local file to BigQuery
+    bigquery_hook.run_load(
+        '/tmp/temp_file.csv',
+        destination_project_dataset_table=f'{dataset_id}.{table_id}',
+        source_format='CSV',
+        autodetect=True,
+        write_disposition='WRITE_TRUNCATE',
+        schema_update_options=['ALLOW_FIELD_ADDITION'],
+    )
+
+with DAG('load_file_to_bigquery', default_args=default_args, schedule_interval=None) as dag:
+    load_to_bigquery = PythonOperator(
+        task_id='load_to_bigquery',
+        python_callable=load_gcs_to_bigquery,
+        op_kwargs={
+            'bucket_name': 'st-landing-bucket',
+            'source_object': 'legislaturas.csv',
+            'dataset_id': 'data',
+            'table_id': 'legislaturas',
+        }
     )
 
     start = DummyOperator(task_id='start')
