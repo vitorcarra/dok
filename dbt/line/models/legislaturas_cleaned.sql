3ac8bea68ce010253b89b3949c3b533fb118bc84
Vitor Carra <vitor.carra@gmail.com>
2024-03-19 10:51:01 -0300

    rename dbt columns

--- dbt/line/models/legislaturas_cleaned.sql
+++ dbt/line/models/legislaturas_cleaned.sql
@@ -4,5 +4,10 @@
     alias='legislaturas_cleaned'
 ) }}
 
-SELECT *
+SELECT
+    string_field_0 as id_legislatura,
+   string_field_1 as sigla_casa,
+   string_field_2 as num_legislatura,
+   string_field_3 as data_inicio,
+   string_field_4 as data_fim
 FROM {{ source('data', 'legislaturas') }}
